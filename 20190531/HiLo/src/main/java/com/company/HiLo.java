4acbf9e0c064f0344266db3c49ced78932135427
unknown <prit.solanki@gmail.com>
2019-06-02 12:17:33 -0400

    Submitting assignment for 06/03/2019

--- 20190531/HiLo/src/main/java/com/company/HiLo.java
+++ 20190531/HiLo/src/main/java/com/company/HiLo.java
@@ -1,14 +1,16 @@
 package com.company;
 import java.util.Scanner;
-import java.uti.Random;
+import java.util.Random;
 
 public class HiLo {
     public static void main(String[] args) {
+        Scanner myScanner = new Scanner(System.in);
         System.out.println("Welcome to Hi-Lo : ");
         System.out.println("Enter Your Name : ");
         System.out.println("Ok, Here are the rules");
 
         Random computerRandom = new Random();
+        int computerGuess;
         computerGuess = computerRandom.nextInt(100) + 1;
         System.out.println(computerGuess);
 
@@ -21,12 +23,14 @@ public class HiLo {
         System.out.println("Guess any number :  (bewtween 1 to 100)");
         userInput = Integer.parseInt(myScanner.nextLine());
         guesses ++;
-             if (userInt < computerRandom){
+
+
+             if (userInput < computerRandom){
                     System.out.println("Too Low");
-             }else if (userInt > computerRandom) {
+             }else if (userInput > computerRandom) {
                     System.out.println("Too high");
              }
-        }while(userInput != computerRandom)
+        }while(userInput != computerRandom);
 
         System.out.println("Congratulation!" + name + "you won!");
         System.out.println("It took you " + guesses + "to guess the number");
