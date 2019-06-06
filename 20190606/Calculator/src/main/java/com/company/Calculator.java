7bab5d208ddd51c5e0a26f2998fcfc12ae48f0fc
unknown <prit.solanki@gmail.com>
2019-06-06 11:20:34 -0400

    Adding homework for Lesson-3 & Lesson-4

---
+++ 20190606/Calculator/src/main/java/com/company/Calculator.java
@@ -0,0 +1,33 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class Calculator {
+
+    public static void main(String[] args) {
+
+        Calculator calc = new Calculator();
+
+    }
+
+
+   public static int add(int no1, int no2) {
+
+        return no1 + no2;
+    }
+
+    public static int subtract(int no1, int no2) {
+
+        return no1 - no2;
+    }
+
+    public static int multiply(int no1, int no2) {
+        return no1 * no2;
+    }
+
+    public static int divide(int no1, int no2) {
+
+        return no1 / no2;
+
+    }
+}
